0f856f29c45e5e3ab918e7b0848850cadc11fc79
Jakub <jakub.lunarzewski@gmail.com>
2022-12-06 21:28:09 +1100

    Day 6 Complete

---
+++ 2022/Day 6/challenge.py
@@ -0,0 +1,19 @@
+def challenge_1():
+    packet = open('challenge_input.txt').read()
+    for i, c in enumerate(packet):
+        if len(set(packet[i:i+4])) == len(packet[i:i+4]):
+            print(i + 4)
+            return
+
+
+def challenge_2():
+    packet = open('challenge_input.txt').read()
+    for i, c in enumerate(packet):
+        if len(set(packet[i:i + 14])) == len(packet[i:i + 14]):
+            print(i + 14)
+            return
+
+
+if __name__ == '__main__':
+    challenge_1()
+    challenge_2()
