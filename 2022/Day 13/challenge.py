979c0982812c2ebf3f0b59ac56c9f6208ecc33af
Jakub <jakub.lunarzewski@gmail.com>
2022-12-17 14:25:37 +1100

    Day 13 WIP

--- 2022/Day 13/challenge.py
+++ 2022/Day 13/challenge.py
@@ -1,2 +1,10 @@
+def challenge_1(pairs):
+    right_order_list = []
+    for i, pair in enumerate(pairs):
+
+
+
 if __name__ == '__main__':
-    print('howdy')
+    string_p = [pair.split('\n') for pair in open('challenge_input.txt').read().split('\n\n')]
+    p_s = [[eval(s[0]), eval(s[1])]for s in string_p]
+    challenge_1(p_s)
